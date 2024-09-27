695c0d94ba5b20c0ebe3b4ea55564c013cc9456c
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-09-27 22:42:39 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "region" {
   description = "The AWS region to deploy resources"
-  type        = string  
-  default     = "eu-north-1" 
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket"
-  type        = string  
+  type        = string
 }
