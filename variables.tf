5d66e68162a51efdd0811098729fec63f157d90a
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-07 18:51:16 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,23 @@ variable "region" {
   description = "The AWS region to deploy resources"
   default     = "eu-north-1"
 }
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for the first public subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for the second public subnet"
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for the first private subnet"
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for the second private subnet"
+  default     = "10.0.4.0/24"
+}
