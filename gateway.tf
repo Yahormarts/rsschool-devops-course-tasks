edd5820e31c5b3831c875ce6795940fa093ee1c5
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-07 19:18:44 +0200

    Create gateway.tf

---
+++ gateway.tf
@@ -0,0 +1,18 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  tags = {
+    Name = "nat_gateway"
+  }
+}
