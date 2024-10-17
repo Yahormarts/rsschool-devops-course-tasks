76aef8a9f9471fd7bf4a28b0e2315af912761f63
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-17 18:50:08 +0200

    Update k8s-cluster.tf

--- k8s-cluster.tf
+++ k8s-cluster.tf
@@ -2,16 +2,24 @@ resource "aws_instance" "k3s_master" {
   ami           = "ami-089146c5626baa6bf"  # AMI for Ubuntu 
   instance_type = "t3.micro"      # Free tier
   subnet_id     = aws_subnet.public_subnet_1.id
-  key_name      = "deploy_key"
+  key_name      = "deploy_key"    # SSH key name
 
   tags = {
     Name = "k3s-master"
   }
 
+  # Подключение по SSH
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"        # default user Ubuntu
+    private_key = file("~/.ssh/id_rsa")  # private key patch GitHub Actions runner
+    host        = self.public_ip  # public IP of created instance
+  }
+
   provisioner "remote-exec" {
     inline = [
       "curl -sfL https://get.k3s.io | sh -",  # Install k3s
-      "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml"  # Configure kubectl
+      "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml"  # Setting kubectl
     ]
   }
 }
@@ -21,11 +29,18 @@ resource "aws_instance" "k3s_worker" {
   ami           = "ami-089146c5626baa6bf"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private_subnet_1.id
-  key_name      = "deploy_key"
-
+  key_name      = "deploy_key"  # SSH key name
 
   tags = {
     Name = "k3s-worker-${count.index}"
+  }
+
+  # Подключение по SSH
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"        # default user Ubuntu
+    private_key = file("~/.ssh/id_rsa")  # private key patch GitHub Actions runner
+    host        = self.public_ip  # public IP of created instance
   }
 
   provisioner "remote-exec" {
