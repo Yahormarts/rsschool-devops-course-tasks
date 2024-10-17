22175e2135115fc2b05b6cfb74383497ac51dcec
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-17 21:31:02 +0200

    Update k8s-cluster.tf

--- k8s-cluster.tf
+++ k8s-cluster.tf
@@ -1,51 +1,51 @@
 resource "aws_instance" "k3s_master" {
-  ami           = "ami-089146c5626baa6bf"  # AMI for Ubuntu 
-  instance_type = "t3.micro"      # Free tier
+  ami           = "ami-089146c5626baa6bf" 
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet_1.id
-  key_name      = "deploy_key"    # SSH key name
+  key_name      = "deploy_key"
+  security_groups = [aws_security_group.k3s_sg.name]
 
   tags = {
     Name = "k3s-master"
   }
 
-  # Подключение по SSH
   connection {
     type        = "ssh"
-    user        = "ubuntu"        # default user Ubuntu
-    private_key = file("~/.ssh/id_rsa")  # private key patch GitHub Actions runner
-    host        = self.public_ip  # public IP of created instance
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | sh -",  # Install k3s
-      "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml"  # Setting kubectl
+      "curl -sfL https://get.k3s.io | sh -",
+      "cat /var/lib/rancher/k3s/server/node-token > /tmp/k3s_token"
     ]
   }
 }
 
 resource "aws_instance" "k3s_worker" {
-  count         = 2 
+  count         = 2
   ami           = "ami-089146c5626baa6bf"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private_subnet_1.id
-  key_name      = "deploy_key"  # SSH key name
+  key_name      = "deploy_key"
+  security_groups = [aws_security_group.k3s_sg.name]
 
   tags = {
     Name = "k3s-worker-${count.index}"
   }
 
-  # Подключение по SSH
   connection {
     type        = "ssh"
-    user        = "ubuntu"        # default user Ubuntu
-    private_key = file("~/.ssh/id_rsa")  # private key patch GitHub Actions runner
-    host        = self.public_ip  # public IP of created instance
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.private_ip
   }
 
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-master:6443 K3S_TOKEN=<your-token> sh -"
+      "K3S_TOKEN=$(curl -s ${aws_instance.k3s_master.public_ip}/tmp/k3s_token) curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_master.private_ip}:6443 K3S_TOKEN=$K3S_TOKEN sh -"
     ]
   }
 }
