24bd6dc57c701f0575783312deebf7a695554990
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-17 21:32:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,36 +43,31 @@ resource "aws_network_acl" "main_acl" {
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
+resource "aws_security_group" "k3s_sg" {
   vpc_id = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]  # или укажите ваш IP
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 6443
+    to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
 
   tags = {
     Name = "allow_ssh"
