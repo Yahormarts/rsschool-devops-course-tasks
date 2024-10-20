5ae880f17b49f17a1b9a115ca90bc413f9388d34
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-20 14:20:08 +0200

    Update k8s-cluster.tf

--- k8s-cluster.tf
+++ k8s-cluster.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "k3s_master" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet_1.id
   key_name      = "deploy_key"
-  security_groups = [aws_security_group.k3s_sg.name]
+  security_groups = [aws_security_group.allow_ssh.name]
 
   tags = {
     Name = "k3s-master"
