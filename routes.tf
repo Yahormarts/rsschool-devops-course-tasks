e230c78500c1b43aa80dce5e6d5968b3a9d25b7f
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-20 19:50:50 +0200

    Update routes.tf

--- routes.tf
+++ routes.tf
@@ -1,57 +1,41 @@
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.public_subnet_1_cidr
-  availability_zone       = "eu-north-1a"
-  map_public_ip_on_launch = true
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = {
-    Name = "public_subnet_1"
+    Name = "public_rt"
   }
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.public_subnet_2_cidr
-  availability_zone       = "eu-north-1b"
-  map_public_ip_on_launch = true
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
   tags = {
-    Name = "public_subnet_2"
+    Name = "private_rt"
   }
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = var.private_subnet_1_cidr
-  availability_zone = "eu-north-1a"
-  tags = {
-    Name = "private_subnet_1"
-  }
+resource "aws_route_table_association" "private_assoc_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = var.private_subnet_2_cidr
-  availability_zone = "eu-north-1b"
-  tags = {
-    Name = "private_subnet_2"
-  }
-}
-
-resource "aws_network_acl_association" "public_acl_association_1" {
-  subnet_id     = aws_subnet.public_subnet_1.id
-  network_acl_id = aws_network_acl.main_acl.id
-}
-
-resource "aws_network_acl_association" "public_acl_association_2" {
-  subnet_id     = aws_subnet.public_subnet_2.id
-  network_acl_id = aws_network_acl.main_acl.id
-}
-
-resource "aws_network_acl_association" "private_acl_association_1" {
-  subnet_id     = aws_subnet.private_subnet_1.id
-  network_acl_id = aws_network_acl.main_acl.id
-}
-
-resource "aws_network_acl_association" "private_acl_association_2" {
-  subnet_id     = aws_subnet.private_subnet_2.id
-  network_acl_id = aws_network_acl.main_acl.id
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
 }
