f7f732ceb00b8fcb6a02e8d9611f26c52e1ce532
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-20 15:09:11 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,3 +22,8 @@ variable "private_subnet_2_cidr" {
   description = "CIDR block for private subnet 2"
   default     = "10.0.4.0/24"
 }
+
+variable "aws_private_key" {
+  description = "Private SSH key for connecting to EC2 instances"
+  type        = string
+}
