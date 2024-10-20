835920ab846aabf987fbe9d04dea62228eb86ceb
Yahormarts <92250063+Yahormarts@users.noreply.github.com>
2024-10-20 20:40:44 +0200

    Update k8s-cluster.tf

--- k8s-cluster.tf
+++ k8s-cluster.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "bastion" {
   ami           = "ami-089146c5626baa6bf"  
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet_1.id
+  associate_public_ip_address = true 
   key_name      = "deploy_key"
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
   
