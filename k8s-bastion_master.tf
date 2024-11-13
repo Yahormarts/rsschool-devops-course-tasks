7f8f2cf3d6383af554b2bee2b64fe28f5a3663cb
Yahor <yahormarts@gmail.com>
2024-11-13 21:16:58 +0100

    Task_3 Change host on private

--- k8s-bastion_master.tf
+++ k8s-bastion_master.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "k3s_master" {
     bastion_host = aws_instance.bastion.public_ip
     agent       = false
     private_key = var.aws_private_key
-    host        = self.public_ip
+    host        = self.private_ip
     timeout = "2m"
   }
 
