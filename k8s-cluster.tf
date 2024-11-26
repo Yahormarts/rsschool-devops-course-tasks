05e1558c6a5a7d290059871a15c9530bc4d95d5c
Yahor <yahormarts@gmail.com>
2024-11-26 19:34:31 +0100

    Task_3 Test

--- k8s-cluster.tf
+++ k8s-cluster.tf
@@ -29,39 +29,34 @@ resource "aws_instance" "k3s_master" {
     bastion_host = aws_instance.bastion.public_ip
     agent       = false
     private_key = var.aws_private_key
-    host        = aws_instance.k3s_master.private_ip
-    timeout = "10m"
+    host        = self.private_ip
+    timeout     = "10m"
   }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      set -x
-      sleep 60
-      echo 'Checking internet connectivity'
-      curl -I https://www.google.com || { echo 'No internet connectivity'; exit 1; }
-      echo 'Starting K3s installation'
-      sleep 60
-      curl -sfL https://get.k3s.io | sh - || { echo 'K3s installation failed'; exit 1; }
-      echo 'K3s installed'
-      sleep 60
-      echo 'Checking K3s installation'
-      /usr/local/bin/k3s --version || { echo 'K3s not found'; exit 1; }
-      sleep 120
-      /usr/local/bin/k3s check-config || { echo 'K3s configuration check failed'; exit 1; }
-      cat /var/lib/rancher/k3s/server/node-token > /tmp/k3s_token
-      echo 'K3s installation complete'
-    EOT
+  provisioner "remote-exec" {
+    inline = [
+      "set -x",
+      "echo 'Checking internet connectivity'",
+      "curl -I https://www.google.com || { echo 'No internet connectivity'; exit 1; }",
+      "echo 'Starting K3s installation'",
+      "curl -sfL https://get.k3s.io | sh - || { echo 'K3s installation failed'; exit 1; }",
+      "echo 'K3s installed'",
+      "/usr/local/bin/k3s --version || { echo 'K3s not found'; exit 1; }",
+      "/usr/local/bin/k3s check-config || { echo 'K3s configuration check failed'; exit 1; }",
+      "cat /var/lib/rancher/k3s/server/node-token > /tmp/k3s_token",
+      "echo 'K3s installation complete'"
+    ]
   }
 }
 
 resource "aws_instance" "k3s_worker" {
   depends_on = [aws_instance.k3s_master]
-  count                       = 2
-  ami                         = "ami-070fe338fb2265e00"
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.private_subnet_1.id
-  key_name                    = "deploy_key"
-  vpc_security_group_ids      = [aws_security_group.k3s_sg.id]
+  count       = 2
+  ami         = "ami-070fe338fb2265e00"
+  instance_type = "t3.micro"
+  subnet_id   = aws_subnet.private_subnet_1.id
+  key_name    = "deploy_key"
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
 
   tags = {
     Name = "k3s-worker-${count.index}"
@@ -73,13 +68,12 @@ resource "aws_instance" "k3s_worker" {
     bastion_host = aws_instance.bastion.public_ip
     agent       = false
     private_key = var.aws_private_key
-    host        = aws_instance.k3s-worker-1.private_ip 
-    timeout = "10m"
+    host        = self.private_ip
+    timeout     = "10m"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sleep 60",
       "K3S_TOKEN=$(curl -s http://${aws_instance.k3s_master.public_ip}/tmp/k3s_token) curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_master.private_ip}:6443 K3S_TOKEN=$K3S_TOKEN sh -"
     ]
   }
